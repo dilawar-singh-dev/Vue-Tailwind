df56891afc049e48397f9e5d83138f8a43db1881
Dilawar <dilawersingh746@gmail.com>
2021-05-23 22:40:30 +0530

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home text-center">
+    <img alt="Vue logo" class="mx-auto" src="../assets/logo.png">
+    <button class="btn ring-4 ring-yellow-500 ring-opacity-50">
+  Button
+</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
